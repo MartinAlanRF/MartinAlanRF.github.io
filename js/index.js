dadeaaec26d9cf44dfd8f0d3542830e71c45b3f3
MartinAlanRF <marodriguez.dwfc6@ucamp.utel.edu.mx>
2022-05-28 16:13:07 -0500

    Modificación de alertas a campos vacios en indeJs

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//Metodo para mostrar los mangas almacenados que se encuentren con el localstorage y mostralos en una table
 let mostrarMangas = () => {
     mangasArr = JSON.parse(localStorage.getItem('mangasArr'));
     if (mangasArr === null) {
@@ -45,7 +46,7 @@ let mostrarMangas = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                         <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                    </svg>    
+                    </svg>     
                 </button>
             </div>
         </td>`;
@@ -53,28 +54,26 @@ let mostrarMangas = () => {
     });
 
 }
-
-
-
+//Metodo para elominar el registro seleccionado con su posición en el arreglo
 let eliminarRegistro = (idIndex) => {
     mangasAlmacenados = JSON.parse(localStorage.getItem('mangasArr'));
     mangasAlmacenados = mangasArr.filter((mangas, index) => index !== idIndex);
     localStorage.setItem('mangasArr', JSON.stringify(mangasAlmacenados));
     mostrarMangas();
 }
-
+//Metodo para rellenar el registro seleccionado con su posición en el arreglo y mostrar la información en el formulario
 let rellenarFormulario = (idIndex) => {
     mangasAlmacenados = JSON.parse(localStorage.getItem('mangasArr'));
     document.getElementById('idIndex').value = idIndex;
     document.getElementById('nombreManga').value = mangasAlmacenados[idIndex].nombre;
     document.getElementById('autorManga').value = mangasAlmacenados[idIndex].autor;
 }
-
+//Metodo para actualizar el registro seleccionado
 let actualizarRegistro = () => {
     let idManga = document.getElementById('idIndex').value;
     let mangaName = document.getElementById('nombreManga').value;
     let mangaAutor = document.getElementById('autorManga').value;
-
+    // Verifica que si se encuentre con un valor el campo escondido id para poder actualizarlo sino este manda una alerta al usuario
     if (idManga == '' || isNaN(idManga)) {
         alert('Debe seleccionar un registro');
         return false;
@@ -85,28 +84,46 @@ let actualizarRegistro = () => {
         nombre: mangaName,
         autor: mangaAutor
     }
-
+    //Se limpian los campos una vez se haya realizado la actualización de un manga en el arreglo
     document.getElementById('idIndex').value = '';
     document.getElementById('nombreManga').value = '';
     document.getElementById('autorManga').value = '';
-
+   //Se guarda en local storage el objeto modificando reescribiendo sus valores del objeto
     localStorage.setItem('mangasArr', JSON.stringify(mangasAlmacenados));
+    //Se manda a llmar la función de mostrar Manga para que se recargue con la información actualizada 
     mostrarMangas();
 }
-
+//Metodo para registrar mangas
 let agregarRegistro = () => {
+    //Se toman los campos por su id y  se extraen los valores con el .value
+    let nombre = document.getElementById('nombreManga').value;
+    let autor = document.getElementById('autorManga').value;
     
+    //Condicional para verificar que los campos esten vacíos
+    if (nombre === '' || autor === '') {
+        //Si el campo vació es el nombre mostrará este error
+        if (nombre === '') {
+            alert('¡Por favor llene el campo del nombre del manga!');
+        }
+        //Si el campo vació es el autor mostrará este error
+        if (autor === '') {
+            alert('¡Por favor llene el campo del autrode del manga!');
+        }
+        //Pongo este return para que se salga de la función y no meta datos vacios al arreglo
+        return;
+    }
+    //Se conviert
     mangasArr = JSON.parse(localStorage.getItem('mangasArr'));
     const nuevoManga = {
-        nombre: document.getElementById('nombreManga').value,
-        autor: document.getElementById('autorManga').value
+        nombre: nombre,
+        autor: autor
     }
-    // console.log(nuevoManga);
     document.getElementById('nombreManga').value = '';
     document.getElementById('autorManga').value = '';
     mangasArr.push(nuevoManga);
     localStorage.setItem('mangasArr', JSON.stringify(mangasArr));
     mostrarMangas();
 }
+
 //Este metodo se manda a llmar para poder que cada que cargue o se refresque la pagina se muestre la lista de los datos de mangasArray
 mostrarMangas();
